3bbbafe96ddf981f74a11fd0c82bce38017dcafd
Ajroudi <mohammed.ajroudi@dxc.com>
2024-01-17 22:35:14 +0000

    first pass

--- blog.config.js
+++ blog.config.js
@@ -2,8 +2,8 @@ const BLOG = {
   title: "MAJROUDI",
   author: "Mohammed AJROUDI",
   email: "ajroudi.mohammed@gmail.com",
-  link: "https://nobelium.vercel.app",
-  description: "This gonna be an awesome website.",
+  link: "https://ajroudilabs.com",
+  description: "AJROUDI MOHAMMED | a developer and a thinker.",
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: "light", // ['light', 'dark', 'auto'],
   font: "sans-serif", // ['sans-serif', 'serif']
@@ -19,7 +19,7 @@ const BLOG = {
   ogImageGenerateURL: "https://og-image-craigary.vercel.app", // The link to generate OG image, don't end with a slash
   socialLink: "https://twitter.com/craigaryhart",
   seo: {
-    keywords: ["Blog", "Website", "Notion"],
+    keywords: ["Ajroudi Mohammed", "Laravel developer", "Architecture", "Design patterns"],
     googleSiteVerification: "", // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
